d68e1717efbf81b9e3fc553ea8b215e6e83afc17
Kevin <61233166+kevinoh45TuD@users.noreply.github.com>
2024-11-18 08:05:09 +0000

    Edit: Heroku allowed hosts fix ?

--- clothes_website/settings.py
+++ clothes_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://pp5-clothes-website-5111351d0c7a.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pp5-clothes-website-5111351d0c7a.herokuapp.com', 'localhost']
 
 
 # Application definition
